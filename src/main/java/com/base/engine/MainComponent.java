c6e68f767c023be8dd6093da52e03c3b90622bda
Thundzz <yacinenew@gmail.com>
2018-03-24 17:38:43 +0100

    handle time

--- src/main/java/com/base/engine/MainComponent.java
+++ src/main/java/com/base/engine/MainComponent.java
@@ -6,36 +6,101 @@ public class MainComponent {
     private static final int HEIGHT = 600;
     private static final String TITLE= "3D Engine";
 
+    private static final double FRAME_CAP = 5000.0;
+
+    private boolean isRunning;
+    private Game game;
+
     public MainComponent()
     {
-
+        isRunning = false;
+        game = new Game();
     }
 
     private void start()
     {
+        if(isRunning)
+            return;
+
         run();
     }
 
     public void stop()
     {
+        if(!isRunning)
+            return;
 
+        isRunning = false;
     }
 
     private void run()
     {
-        while(!Window.isCloseRequested()){
-            render();
+        isRunning = true;
+        int frames = 0;
+        long frameCounter = 0;
+
+        final double frameTime = 1.0 / FRAME_CAP;
+
+        long lastTime = Time.getTime();
+        double unprocessedTime = 0;
+
+
+        while(isRunning)
+        {
+            boolean render = false;
+            long startTime = Time.getTime();
+            long passedTime = startTime - lastTime;
+            lastTime = startTime;
+
+            unprocessedTime += passedTime/ (double) Time.SECOND;
+            frameCounter += passedTime;
+
+            while(unprocessedTime > frameTime)
+            {
+                render = true;
+                unprocessedTime -= frameTime;
+                if(Window.isCloseRequested())
+                    stop();
+
+                Time.setDelta(frameTime);
+
+                game.input();
+                game.update();
+
+                if(frameCounter >= Time.SECOND)
+                {
+                    System.out.println(frames);
+                    System.out.println(frameCounter);
+                    frames = 0;
+                    frameCounter = 0;
+                }
+            }
+            if(render) {
+                render();
+                frames++;
+            }
+            else
+            {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+        cleanUp();
     }
 
     private void render()
     {
+        game.render();
         Window.render();
     }
 
-    public void cleanUp()
+    private void cleanUp()
     {
-
+        Window.dispose();
     }
 
     public static void main(String[] args)
