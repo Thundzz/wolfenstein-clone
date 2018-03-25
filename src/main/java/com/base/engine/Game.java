82fde278adc0e0a0baac4d42b6acf953de8aec61
Thundzz <yacinenew@gmail.com>
2018-03-25 16:53:31 +0200

    basic mesh rendering

--- src/main/java/com/base/engine/Game.java
+++ src/main/java/com/base/engine/Game.java
@@ -4,9 +4,18 @@ import org.lwjgl.input.Keyboard;
 
 public class Game {
 
+    private Mesh mesh;
+
     public Game()
     {
+        mesh = new Mesh();
+        Vertex[] data = new Vertex[]{
+                new Vertex(new Vector3f(-1, -1, 0)),
+                new Vertex(new Vector3f(0,  1, 0)),
+                new Vertex(new Vector3f(1, -1, 0)),
 
+        };
+        mesh.addVertices(data);
     }
 
     public void input()
@@ -31,7 +40,7 @@ public class Game {
 
     public void render()
     {
-
+        mesh.draw();
     }
 
 }
