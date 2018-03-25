019067912d6ec30522b034decf023a6ee3b09e8b
Thundzz <yacinenew@gmail.com>
2018-03-25 18:10:24 +0200

    implement translation

--- src/main/java/com/base/engine/Game.java
+++ src/main/java/com/base/engine/Game.java
@@ -8,6 +8,7 @@ public class Game {
 
     private Mesh mesh;
     private Shader shader;
+    private Transform transform;
 
     public Game()
     {
@@ -22,14 +23,16 @@ public class Game {
         };
 
         mesh.addVertices(data);
+
+        transform = new Transform();
+
         shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vs"));
         shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.fs"));
 
 
         shader.compileShader();
-        shader.bind();
 
-        shader.addUniform("uniformFloat");
+        shader.addUniform("transform");
     }
 
     public void input()
@@ -51,12 +54,15 @@ public class Game {
     public void update()
     {
         temp += Time.getDelta();
-        shader.setUniformf("uniformFloat", (float)Math.abs(Math.sin(temp)));
+        transform.setTranslation((float) Math.sin(temp), 0, 0);
     }
 
     public void render()
     {
+        shader.bind();
+        shader.setUniform("transform", transform.getTransformation());
         mesh.draw();
+
     }
 
 }
