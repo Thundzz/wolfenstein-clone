b674c722da36ea19a7f13f7eac038e3fa594968d
Thundzz <yacinenew@gmail.com>
2018-03-31 23:16:01 +0200

    basic load obj meshes

--- src/main/java/com/base/engine/Game.java
+++ src/main/java/com/base/engine/Game.java
@@ -10,10 +10,10 @@ public class Game {
 
     public Game()
     {
-        mesh = new Mesh();
+        mesh = ResourceLoader.loadMesh("box.obj");
         shader = new Shader();
 
-        Vertex[] vertices = new Vertex[]{
+     /*   Vertex[] vertices = new Vertex[]{
                 new Vertex(new Vector3f(-1, -1, 0)),
                 new Vertex(new Vector3f(0,  1, 0)),
                 new Vertex(new Vector3f(1, -1, 0)),
@@ -25,7 +25,7 @@ public class Game {
                                     2, 1, 0,
                                     0, 2, 3  };
 
-        mesh.addVertices(vertices, indices);
+        mesh.addVertices(vertices, indices);*/
 
         transform = new Transform();
 
@@ -62,7 +62,7 @@ public class Game {
 
         transform.setTranslation(sinTemp, 0, 0);
         transform.setRotation(0, sinTemp * 180, 0);
-        //transform.setScale(sinTemp, sinTemp, sinTemp);
+        transform.setScale(0.7f*sinTemp, 0.7f*sinTemp, 0.7f*sinTemp);
 
     }
 
