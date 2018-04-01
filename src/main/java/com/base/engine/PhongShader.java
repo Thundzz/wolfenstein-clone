6e19424c1b417050b010cbab13669ce6f07d7d00
Thundzz <yacinenew@gmail.com>
2018-04-01 23:20:42 +0200

    Optimize point lights

--- src/main/java/com/base/engine/PhongShader.java
+++ src/main/java/com/base/engine/PhongShader.java
@@ -46,6 +46,7 @@ public class PhongShader extends Shader
             addUniform("pointLights["+ i +"].atten.linear");
             addUniform("pointLights["+ i +"].atten.exponent");
             addUniform("pointLights["+ i +"].position");
+            addUniform("pointLights["+ i +"].range");
         }
     }
 
@@ -119,5 +120,6 @@ public class PhongShader extends Shader
         setUniformf(uniformName + ".atten.linear" , pointLight.getAtten().getLinear());
         setUniformf(uniformName + ".atten.exponent" , pointLight.getAtten().getExponent());
         setUniform(uniformName + ".position" , pointLight.getPosition());
+        setUniformf(uniformName + ".range" , pointLight.getRange());
     }
 }
